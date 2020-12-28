31ff9b508138a7e575f298612a1e2a59cb6eb1c8
abdelrahman <abdelrahman@toptal.com>
2020-12-28 06:15:45 +0200

    gitignore

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var solve_1 = require("./solve");
-var g = new solve_1.Grid();
-window.alert("hello");
-if (typeof (window) !== "undefined") {
-    window.alert("hello");
-}
